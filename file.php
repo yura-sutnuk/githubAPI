562e74944810e9da78e0fa1a7519e53ca85e77d5
yura-sutnuk <Sutnuk.Yura@bigmir.net>
2018-02-01 20:38:06 +0200

    ''

--- file.php
+++ file.php
@@ -1,43 +1,57 @@
 <?php
 
+$form = $_POST['form'];
+$form = json_decode($form);
 
-$size = count($_POST['field']);
-$request = 'q=';
+$size = count($form);
+$request = null;
 for($i=0;$i<$size;$i++)
 {
-    if(!empty($_POST['value'][$i]) )
+    if(empty($form[$i]->value) || empty($form[$i]->field) || empty($form[$i]->operator)  )
+    {
+        continue;
+    }
+    else
     {
         if($i>0)
         {
             $request .= '+';
         }
-        if($_POST['operator'][$i] == '=')
+        if($form[$i]->operator == '=')
         {
-            $_POST['operator'][$i] = '';
+            $form[$i]->operator = '';
         }
 
-        $request .= $_POST['field'][$i].":";
-        $request .= $_POST['operator'][$i];
-        $request .= $_POST['value'][$i];
+        $request .= $form[$i]->field.":";
+        $request .= $form[$i]->operator;
+        $request .= $form[$i]->value;
     }
 }
-
+if($request == null)
+{
+    echo 'Rules are empty';
+    return;
+}
 $connection = curl_init();
-curl_setopt($connection, CURLOPT_URL, "https://api.github.com/search/repositories?{$request}");
+curl_setopt($connection, CURLOPT_URL, "https://api.github.com/search/repositories?q={$request}");
 curl_setopt($connection, CURLOPT_USERAGENT,  "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB7.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C; .NET4.0E)");
 curl_setopt($connection, CURLOPT_RETURNTRANSFER, 1);
+
 $result = json_decode(curl_exec($connection));
+curl_close($connection);
 
 $items = $result->items;
+$response = null;
 foreach($items as $item)
 {
-    echo 'name: '.$item->name.'<br>';
-    echo 'src: '.$item->html_url.'<br>';
-    echo 'size: '.$item->size.'<br>';
-    echo 'forks count: '.$item->forks_count.'<br>';
-    echo 'stars: '.$item->stargazers_count.'<br>';
 
-    echo '<br><br>';
+    $response .= 'name: '.$item->name.'<br>';
+    $response .= 'src: '.$item->html_url.'<br>';
+    $response .= 'size: '.$item->size.'<br>';
+    $response .= 'forks count: '.$item->forks_count.'<br>';
+    $response .= 'stars: '.$item->stargazers_count.'<br>';
+
+    $response .= '<br><br>';
 }
 
-curl_close($connection);
+echo $response;
--- script.js
+++ script.js
@@ -3,14 +3,21 @@ function addRule() {
     var form = document.querySelector('#form');
 
     var arr = ['size', 'forks', 'stars', 'followers'];
-    var field = createSelect('field[]', arr);
+    var field = createSelect('field', arr);
+    var option = createPlaceholder('field');
+    field.insertBefore(option, field.children[0]);
+    field.onclick=function(){this.style.color='black'};
 
     arr = ['>', '<', '='];
-    var operator = createSelect('operator[]', arr);
+    var operator = createSelect('operator', arr);
+    var option = createPlaceholder('operator');
+    operator.insertBefore(option, operator.children[0]);
+    operator.onclick=function(){this.style.color='black'};
 
     var value = document.createElement('input');
     value.type = 'number';
-    value.name = 'value[]';
+    value.name = 'value';
+    value.placeholder = 'value';
 
     var id = 1;
     while (document.getElementById(id))
@@ -18,9 +25,9 @@ function addRule() {
         id++;
     }
 
-    var deleteButton = document.createElement('input');
-    deleteButton.type = 'button';
-    deleteButton.value = "Delete";
+    var deleteButton = document.createElement('a');
+    deleteButton.className = 'Delete Button';
+    deleteButton.innerHTML = "Delete";
     deleteButton.onclick= function(){ Delete(id); };
 
     var div = document.createElement('div');
@@ -50,6 +57,16 @@ function createSelect(name, arr)
 
     return select;
 }
+
+function createPlaceholder(text)
+{
+    var option = document.createElement('option');
+    option.value ='';
+    option.selected = true;
+    option.className = 'placeholder';
+    option.innerHTML = text;
+    return option;
+}
 function Clear()
 {
     form = document.querySelector('#form');
@@ -58,8 +75,38 @@ function Clear()
 }
 function submit()
 {
-    form = document.querySelector('#form');
-    form.submit();
+    formData = getFormData();
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if(req.readyState==4 && req.status == 200)
+        {
+            var result  = document.querySelector('#result');
+            result.innerHTML = req.responseText;
+
+            document.getElementById('body').appendChild(result);
+        }
+    }
+
+    req.open("POST","file.php",true);
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    req.send('form='+formData);
+
+}
+
+function getFormData()
+{
+    var form = document.querySelector('#form');
+    var data = new Array();
+    for(var i = 0; i+2 < form.elements.length; i+=3)
+    {
+        var param = new Object();
+        param[form.elements[i].name] = form.elements[i].value;
+        param[form.elements[i+1].name] = form.elements[i+1].value;
+        param[form.elements[i+2].name] = form.elements[i+2].value;
+        data.push(param);
+    }
+
+    return JSON.stringify(data);
 }
 
 function Delete(id)
